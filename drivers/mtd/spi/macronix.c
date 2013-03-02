7c60c9a1f1cd9c20de493d5166e45fed6f84f89a
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2013-03-02 23:09:32 +0100

    sf: macronix: add support for MX25L25635E
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- drivers/mtd/spi/macronix.c
+++ drivers/mtd/spi/macronix.c
@@ -79,6 +79,11 @@ static const struct macronix_spi_flash_params macronix_spi_flash_table[] = {
 		.nr_blocks = 256,
 		.name = "MX25L12855E",
 	},
+	{
+		.idcode = 0x2019,
+		.nr_blocks = 512,
+		.name = "MX25L25635E",
+	},
 };
 
 static __maybe_unused int macronix_set_4byte_mode(struct spi_flash *flash)
