f0bc86bd1ce1b84e061f91be16b19377106fd37a
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2013-03-02 23:11:36 +0100

    net: switch: update or fix copyright headers
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- drivers/net/switch/adm6996i.c
+++ drivers/net/switch/adm6996i.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2012 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ * Copyright (C) 2011-2013 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
  *
  * This file is released under the terms of GPL v2 and any later version.
  * See the file COPYING in the root directory of the source tree for details.
--- include/switch.h
+++ include/switch.h
@@ -2,7 +2,7 @@
  * This file is released under the terms of GPL v2 and any later version.
  * See the file COPYING in the root directory of the source tree for details.
  *
- * Copyright (C) 2011 Daniel Schwierzeck, daniel.schwierzeck@googlemail.com
+ * Copyright (C) 2011-2013 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
  */
 
 #ifndef __SWITCH_H
