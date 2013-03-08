f6750750a83be9fc1ad370035cf48c3f2486f7d3
Daniel Golle <daniel.golle@gmail.com>
2013-03-08 13:29:04 +0200

    MIPS: add board support for AudioCodes MP-252
    Signed-off-by: Daniel Golle <dgolle@allnet.de>

---
+++ board/audiocodes/acmp252/config.mk
@@ -0,0 +1,8 @@
+#
+# This file is released under the terms of GPL v2 and any later version.
+# See the file COPYING in the root directory of the source tree for details.
+#
+# Copyright (C) 2011 Daniel Schwierzeck, daniel.schwierzeck@googlemail.com
+#
+
+PLATFORM_CPPFLAGS += -I$(TOPDIR)/board/$(BOARDDIR)
