be1b12e584fb1a1addeb7478f4383e46c738b18c
Luka Perkov <luka@openwrt.org>
2013-08-17 03:44:46 +0200

    MIPS: lantiq: add default openwrt config
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/openwrt-lantiq-common.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 Luka Perkov <luka@openwrt.org>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __OPENWRT_LANTIQ_COMMON_H
+#define __OPENWRT_LANTIQ_COMMON_H
+
+/* Commands */
+#if defined(CONFIG_LTQ_SUPPORT_ETHERNET)
+#define CONFIG_CMD_PING
+#endif
+
+/* Compression */
+#define CONFIG_LZMA
+
+/* Auto boot */
+#define CONFIG_BOOTDELAY	2
+
+/* Environment configuration */
+#if !defined(CONFIG_SYS_BOOT_RAM)
+#define CONFIG_BOOTCOMMAND \
+	"bootm ${kernel_addr}"
+#endif
+
+/* Temporary ethernet configuration */
+#if defined(CONFIG_LTQ_SUPPORT_ETHERNET)
+#define CONFIG_ETHADDR		00:01:02:03:04:05
+#define CONFIG_SERVERIP		192.168.1.2
+#define CONFIG_IPADDR		192.168.1.1
+#endif
+
+#endif /* __OPENWRT_LANTIQ_COMMON_H */
