eb40aa1733c344a8cac7735011b8f11d913dbe62
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2014-02-09 01:45:18 +0100

    MIPS: vrx200: fix for kbuild
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/cpu/mips32/vrx200/config.mk
+++ arch/mips/cpu/mips32/vrx200/config.mk
@@ -14,22 +14,22 @@ PF_PIE			:=
 USE_PRIVATE_LIBGCC	:= yes
 endif
 
-LIBS-y += $(CPUDIR)/lantiq-common/
+libs-y += $(CPUDIR)/lantiq-common/
 
 ifndef CONFIG_SPL_BUILD
 ifdef CONFIG_SYS_BOOT_SFSPL
-ALL-y += $(obj)u-boot.ltq.sfspl
-ALL-$(CONFIG_SPL_LZO_SUPPORT) += $(obj)u-boot.ltq.lzo.sfspl
-ALL-$(CONFIG_SPL_LZMA_SUPPORT) += $(obj)u-boot.ltq.lzma.sfspl
+ALL-y += u-boot.ltq.sfspl
+ALL-$(CONFIG_SPL_LZO_SUPPORT) += u-boot.ltq.lzo.sfspl
+ALL-$(CONFIG_SPL_LZMA_SUPPORT) += u-boot.ltq.lzma.sfspl
 endif
 ifdef CONFIG_SYS_BOOT_NORSPL
-ALL-y += $(obj)u-boot.ltq.norspl
-ALL-$(CONFIG_SPL_LZO_SUPPORT) += $(obj)u-boot.ltq.lzo.norspl
-ALL-$(CONFIG_SPL_LZMA_SUPPORT) += $(obj)u-boot.ltq.lzma.norspl
+ALL-y += u-boot.ltq.norspl
+ALL-$(CONFIG_SPL_LZO_SUPPORT) += u-boot.ltq.lzo.norspl
+ALL-$(CONFIG_SPL_LZMA_SUPPORT) += u-boot.ltq.lzma.norspl
 endif
 ifdef CONFIG_SYS_BOOT_NANDSPL
-ALL-y += $(obj)u-boot.ltq.nandspl
-ALL-$(CONFIG_SPL_LZO_SUPPORT) += $(obj)u-boot.ltq.lzo.nandspl
-ALL-$(CONFIG_SPL_LZMA_SUPPORT) += $(obj)u-boot.ltq.lzma.nandspl
+ALL-y += u-boot.ltq.nandspl
+ALL-$(CONFIG_SPL_LZO_SUPPORT) += u-boot.ltq.lzo.nandspl
+ALL-$(CONFIG_SPL_LZMA_SUPPORT) += u-boot.ltq.lzma.nandspl
 endif
 endif
