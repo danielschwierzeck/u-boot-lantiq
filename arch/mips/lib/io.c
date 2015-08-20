6bd7026fc2707441f06d032d5dd9e9c6f12f2720
Govindraj Raja <Govindraj.Raja@imgtec.com>
2015-08-20 15:01:32 +0100

    MIPS: add clrbits and setbits and add phy_to_bus support.
    Commit 369d2aad8ac54792118fbf857b94826266e40454 upstream.
    
    usb stack utilizes the clr/set_bits macros
    also usb stack needs phy_to_bus/bus_to_phys functions.
    Thus adding these macro and functions for mips platform.
    
    This makes usb stack usable with mips platform.
    
    Signed-off-by: Govindraj Raja <govindraj.raja@imgtec.com>
    [daniel.schwierzeck@gmail.com: added missing include files]
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/lib/io.c
+++ arch/mips/lib/io.c
@@ -5,8 +5,25 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
+#include <asm/io.h>
+#include <linux/compiler.h>
+
 /*
  * mips_io_port_base is the begin of the address space to which x86 style
  * I/O ports are mapped.
  */
 const unsigned long mips_io_port_base = -1;
+
+#ifdef CONFIG_PHYS_TO_BUS
+
+__weak unsigned long phys_to_bus(unsigned long phys)
+{
+	return (unsigned long)virt_to_phys((void *)phys);
+}
+
+__weak unsigned long bus_to_phys(unsigned long bus)
+{
+	return (unsigned long)phys_to_virt(bus);
+}
+
+#endif
