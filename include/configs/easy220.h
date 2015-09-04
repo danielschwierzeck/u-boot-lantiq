8b183678296f02151c781473218f7f81e064c0b7
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2015-09-04 13:43:58 +0200

    fixup! MIPS: add board support for Lantiq EASY220

--- include/configs/easy220.h
+++ include/configs/easy220.h
@@ -78,12 +78,7 @@
 #include <asm/lantiq/config.h>
 #include <asm/arch/config.h>
 
-
-#define CONFIG_ENV_UPDATE_UBOOT_SF					\
-	"update-uboot-sf=run load-uboot-sfspl-lzo write-uboot-sf\0"
-
 #define CONFIG_EXTRA_ENV_SETTINGS	\
-	CONFIG_ENV_LANTIQ_DEFAULTS	\
-	CONFIG_ENV_UPDATE_UBOOT_SF
+	CONFIG_ENV_LANTIQ_DEFAULTS
 
 #endif /* __CONFIG_H */
