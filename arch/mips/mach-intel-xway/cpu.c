e093c30b704594d77f8d95701af896a814b1d46c
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2015-09-13 21:51:25 +0200

    MIPS: add initial infrastructure for Intel XWAY SoCs
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ arch/mips/mach-intel-xway/cpu.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016-2020 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ */
+
+#include <dm.h>
+#include <log.h>
+#include <ram.h>
+
+int dram_init(void)
+{
+	struct ram_info ram;
+	struct udevice *dev;
+	int err;
+
+	err = uclass_get_device(UCLASS_RAM, 0, &dev);
+	if (err) {
+		debug("DRAM init failed: %d\n", err);
+		return 0;
+	}
+
+	err = ram_get_info(dev, &ram);
+	if (err) {
+		debug("Cannot get DRAM size: %d\n", err);
+		return 0;
+	}
+
+	debug("SDRAM base=%zx, size=%x\n", ram.base, ram.size);
+
+	return ram.size;
+}
---
+++ arch/mips/mach-intel-xway/include/mach/soc.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016-2020 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ */
+
+#ifndef __MACH_INTEL_SOC_H
+#define __MACH_INTEL_SOC_H
+
+enum intel_xway_partnum {
+	INTEL_XWAY_UNKNOWN = 0,
+};
+
+#endif /* __MACH_INTEL_SOC_H */
