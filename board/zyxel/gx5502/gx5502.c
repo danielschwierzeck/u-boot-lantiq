cc1cb9018b506d98a5ca143e0a0eaa122141c2ea
Daniel Schwierzeck <daniel.schwierzeck@zyxel.eu>
2020-07-29 15:56:43 +0200

    MIPS: lantiq: add support for Zyxel GX5502-D0/GX3502-D0 baords

---
+++ board/zyxel/gx5502/gx5502.c
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2020 Sphairon GmbH (a ZyXEL company)
+ */
+
+#include <common.h>
+#include <mach/gpio.h>
+#include <sas/controlfile.h>
+
+static void gx5502_gpio_init(void)
+{
+	/* SLIC reset (low-active) */
+	gpio_direction_output(1, 1);
+	/* PCI-E 0 reset (low-active) */
+	gpio_direction_output(14, 1);
+	/* PCI-E 1 reset (low-active) */
+	gpio_direction_output(3, 1);
+	/* PCI-E 2 reset (high-active) */
+	gpio_direction_output(2, 0);
+
+	/* USB port OC (high-active) */
+	gpio_direction_output(7, 0);
+	/* USB port power (high-active) */
+	gpio_direction_output(9, 0);
+
+	/* Wifi power (high-active) */
+	gpio_direction_output(61, 0);
+
+	/* NAND CS0 (controlled manually via GPIO, low-active) */
+	gpio_direction_output(8, 1);
+	/* NAND CS1 (low-active) */
+	ltq_gpio_mux(23, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND ALE (high-active) */
+	ltq_gpio_mux(13, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND CLE (high-active) */
+	ltq_gpio_mux(24, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND WE (low-active) */
+	ltq_gpio_mux(59, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND WP (low-active) */
+	ltq_gpio_mux(60, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND RE (low-active) */
+	ltq_gpio_mux(49, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND Ready/Busy */
+	ltq_gpio_mux(48, LTQ_PORTMUX_EBU, LTQ_GPIO_INPUT);
+
+	/* NAND D0 (high-active) */
+	ltq_gpio_mux(51, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D1 (high-active) */
+	ltq_gpio_mux(50, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D2 (high-active) */
+	ltq_gpio_mux(52, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D3 (high-active) */
+	ltq_gpio_mux(57, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D4 (high-active) */
+	ltq_gpio_mux(56, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D5 (high-active) */
+	ltq_gpio_mux(55, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D6 (high-active) */
+	ltq_gpio_mux(54, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+	/* NAND D7 (high-active) */
+	ltq_gpio_mux(53, LTQ_PORTMUX_EBU, LTQ_GPIO_OUTPUT);
+
+	/* Reset button */
+	gpio_direction_input(0);
+	/* WLAN button */
+	gpio_direction_input(42);
+	/* WPS button */
+	gpio_direction_input(43);
+
+	/* SLIC SPI CS (low-active) */
+	gpio_direction_output(10, 1);
+	/* IPAC SPI CS (low-active) */
+	gpio_direction_output(11, 1);
+	/* IPAC SPI CS (low-active) */
+	gpio_direction_output(15, 1);
+
+	/* PAE MDC (low-active) */
+	gpio_direction_output(33, 1);
+	/* PAE MDIO (low-active) */
+	gpio_direction_output(32, 1);
+
+	/* I2C SDA (high-active) */
+	gpio_direction_output(21, 0);
+	/* I2C SCL (high-active) */
+	gpio_direction_output(22, 0);
+	/* I2C SFP SDA (high-active) */
+	gpio_direction_output(34, 0);
+	/* I2C SFP SCL (high-active) */
+	gpio_direction_output(35, 0);
+
+	/* SSO LED_ST */
+	ltq_gpio_mux(4, LTQ_PORTMUX_SSO, LTQ_GPIO_OUTPUT);
+	/* SSO LED_D */
+	ltq_gpio_mux(5, LTQ_PORTMUX_SSO, LTQ_GPIO_OUTPUT);
+	/* SSO LED_SH */
+	ltq_gpio_mux(6, LTQ_PORTMUX_SSO, LTQ_GPIO_OUTPUT);
+
+	/* Enable red:power */
+	ltq_gpio_init(BIT(14));
+}
+
+int board_early_init_f(void)
+{
+	gx5502_gpio_init();
+	return 0;
+}
+
+int sas_cf_check_board(void)
+{
+	/* check if reset button is pressed */
+	return 0 == gpio_get_value(0);
+}
+
+void sas_cf_led_action(enum sas_cf_state state)
+{
+	switch (state) {
+	case CF_STARTED:
+		/* LED Power green on */
+		gpio_set_value(79, 1);
+		break;
+	case CF_FINISHED:
+		/* LED Power green off */
+		gpio_set_value(79, 0);
+		break;
+	case CF_FAILED:
+		/* LED Info red on */
+		gpio_set_value(73, 1);
+		/* LED Power green off */
+		gpio_set_value(79, 0);
+		break;
+	}
+}
---
+++ include/configs/gx5502.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2020 Sphairon GmbH (a ZyXEL company)
+ */
+
+#ifndef _CONFIG_ZYXEL_GX5502_H_
+#define _CONFIG_ZYXEL_GX5502_H_
+
+#define CONFIG_IDENT_STRING		" gx5502"
+#define CONFIG_BOARD_NAME		"Zyxel gx5502 series [620, 621]"
+
+#define CONFIG_SYS_MAX_NAND_DEVICE	2
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+#define CONFIG_SYS_NAND_PAGE_SIZE	0x1000
+#define CONFIG_SYS_NAND_BLOCK_SIZE	0x40000
+#define CONFIG_SYS_NAND_PAGE_COUNT	(CONFIG_SYS_NAND_BLOCK_SIZE / \
+					CONFIG_SYS_NAND_PAGE_SIZE)
+
+#define CONFIG_SPL_U_BOOT_OFFS		0x80000
+#define CONFIG_SPL_U_BOOT_SIZE		0x80000
+#define CONFIG_LTQ_SPL_REDUND_IMAGES	1
+
+#define CONFIG_MTD_UBI_BEB_LIMIT	20
+#define CONFIG_ENV_UBI_VID_HDR_OFF	"4096"
+
+#define CONFIG_BOARD_EARLY_INIT_F
+
+#define MTDIDS_DEFAULT			"nand0=nand-cs1,nand1=nand-cs0"
+#define MTDPARTS_DEFAULT		\
+	"mtdparts=nand-cs1:1792k(uboot_fix),1024k(calibration),-(ubi);nand-cs0:-(storage)"
+
+#include <configs/xrx500_common.h>
+#include <configs/gemini_env.h>
+
+#define CONFIG_EXTRA_ENV_SETTINGS			\
+	CONFIG_ENV_XRX500				\
+	CONFIG_ENV_GEMINI
+
+#endif /* _CONFIG_ZYXEL_GX5502_H_ */
